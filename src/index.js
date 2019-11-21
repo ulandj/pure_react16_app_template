eb97f8e34a97d7812705e768f4e05d9c724102e7
Ulan Djamanbalaev <udjamanbalaev@gmail.com>
2019-11-21 19:32:30 +0600

    Add missed @babel/plugin-proposal-export-default-from plugin to export components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import './style.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Layout from './components/Layout';
+import { Layout } from './components';
 
 ReactDOM.render(<Layout />, document.getElementById('root'));
